5160ab81722cf42b0f7e98cd09421f2eb33f0b3d
Aleem Virani <52095181+aleemv1@users.noreply.github.com>
2023-10-31 15:34:56 -0400

    new particles

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,94 +1,48 @@
 /* ---- particles.js config ---- */
 /* eslint-disable */
-particlesJS("particles-js", {
+particlesJS('particles-js', {
   "particles": {
-    "number": {
-      "value": 90,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#FF69B4"
-    },
+    "number": { "value": 100, "density": { "enable": true, "value_area": 800 } },
+    "color": { "value": "#ffffff" },
     "shape": {
       "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
+      "stroke": { "width": 0, "color": "#000000" },
+      "polygon": { "nb_sides": 5 },
+      "image": { "src": "img/github.svg", "width": 100, "height": 100 }
     },
     "opacity": {
-      "value": 0.7,
+      "value": 0.5,
       "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
+      "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false }
     },
     "size": {
-      "value": 30,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.25,
-        "sync": false
-      }
+      "value": 2,
+      "random": false,
+      "anim": { "enable": false, "speed": 1, "size_min": 0.1, "sync": false }
     },
     "line_linked": {
-      "enable": false,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
+      "enable": true
     },
     "move": {
       "enable": true,
       "speed": 1,
-      "direction": "none",
+      "direction": "random",
       "random": false,
       "straight": false,
       "out_mode": "bounce",
       "bounce": true,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
+      "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 }
     }
   },
   "interactivity": {
     "detect_on": "canvas",
     "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "repulse"
-      },
-      "resize": false
+      "onhover": { "enable": false, "mode": "repulse" },
+      "onclick": { "enable": false, "mode": "push" },
+      "resize": true
     },
     "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
+      "grab": { "distance": 400, "line_linked": { "opacity": 1 } },
       "bubble": {
         "distance": 400,
         "size": 40,
@@ -96,40 +50,13 @@ particlesJS("particles-js", {
         "opacity": 8,
         "speed": 3
       },
-      "repulse": {
-        "distance": 50,
-        "duration": 0.5
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
+      "repulse": { "distance": 200, "duration": 0.4 },
+      "push": { "particles_nb": 4 },
+      "remove": { "particles_nb": 2 }
     }
   },
   "retina_detect": true
-});
-
-
-/* ---- stats.js config ---- */
-
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
+}
+);
 
 /* eslint-enable */
